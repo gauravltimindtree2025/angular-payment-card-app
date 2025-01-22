030a2a538238e8a67d972c3c345fe92bd4e79996
Gaurav Budhiraja <Gaurav.10826095@LTIMindtree.com>
2025-01-22 18:05:30 +0530

    initial commit

---
+++ src/app/PaymentDetails/payment-detail-form/payment-detail-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-detail-form',
+  standalone: false,
+  
+  templateUrl: './payment-detail-form.component.html',
+  styleUrl: './payment-detail-form.component.css'
+})
+export class PaymentDetailFormComponent {
+
+}
